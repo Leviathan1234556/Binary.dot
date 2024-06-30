90c8350599baeecdaef1f15c5add738c083d0e57
R Akash <93132839+Leviathan1234556@users.noreply.github.com>
2024-06-30 15:40:23 +0530

    Update signup.jsx

--- Binarydothomepage/src/signup.jsx
+++ Binarydothomepage/src/signup.jsx
@@ -9,7 +9,7 @@ function Signup() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const navigate = useNavigate()
-
+    axios.defaults.withCredentials = true;
     const handleSubmit = (e) => {
         e.preventDefault()
         axios.post('https://binary-dot.vercel.app/register', { name, email, password })
